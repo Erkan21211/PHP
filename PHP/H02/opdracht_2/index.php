fd633695c9b679bd9f3d6f19efdba2a537b437ed
Erkan <2106563@talnet.nl>
2022-03-13 21:52:24 +0100

    commit 2

---
+++ PHP/H02/opdracht_2/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet";
+
+echo 'Mensen ' . $a . '' . $b . ' ' . 'maken,' . ' ' . 'werken ' . $c . '<br>';
+echo '-Albert Piesman (KLM)';
+
+
+?>
---
+++ PHP/H02/opdracht_3/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$a = 'ry';
+$b = ", but";
+$c = "fail";
+
+echo 't' . $a . ' ' . ' ' . 'but' . ' ' . $c . $b . ' '. 'never' . ' ' . $c . ' ' . 't'  . $a . '<br>';
+echo '- Tyra Banks'
+
+?>
